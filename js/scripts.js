493a612a3a3dd0ca39451363983d98c5cb6df2d2
Kira <rosehill1788@protonmail.com>
2019-06-21 16:29:06 -0700

    Fix backend math for adding ingredien add on costs

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,15 @@ $(document).ready(function() {
   $("#formOrder").submit(function(event) {
     event.preventDefault();
 
-    var inputSize = parseInt$("select#size").val());
+    var inputSize = parseInt($("select#size").val());
     var inputMeat = parseInt($("#meat").val());
     var inputVegetable = parseInt($("#vegetable").val());
     var inputSauce = parseInt($("#sauce").val());
 
     var pizzaOrder = new Pizza(0, 0, 0, 0)
-
     var testPizza = new Pizza(2, 4, 2, 2)
+
+    console.log(checkout);
 
     function Pizza(size, meat, vegetable, sauce) {
       this.size = size,
@@ -24,50 +25,50 @@ $(document).ready(function() {
       this.sauce = sauce
     }
 
-    function pizzaSize(size) {
-      if (size === 2) {
-        return 4
-      } else {
-        return 0
-      }
-    }
-
-    function pizzaMeat(meat) {
-      if (meat === 4) {
-        return 2
-      } else if (meat === 3) {
-        return 2
-      } else if (meat === 2) {
-        return 1
-      } else {
-        return 0
-      }
-    }
-
-    function pizzaVegetable(vegetable) {
-      if (vegetable === 4) {
-        return 2
-      } else if (vegetable === 3) {
-        return 2
-      } else if (vegetable === 2) {
-        return 1
-      } else {
-        return 0
-      }
-    }
-
-    function pizzaSauce(sauce) {
-      if (sauce === 2) {
-        return 2
-      } else {
-        return 1
-      }
-    }
 
     var pizzaTotal = 0
-    var checkout = pizzaTotal;
+    var checkout = inputSize + inputMeat + inputVegetable + inputSauce;
 
     Pizza.prototype.price = function(pizzaOrder) {
+      function pizzaSize(size) {
+        if (size === 2) {
+          return 4
+        } else {
+          return 0
+        }
+      }
+
+      function pizzaMeat(meat) {
+        if (meat === 4) {
+          return 2
+        } else if (meat === 3) {
+          return 2
+        } else if (meat === 2) {
+          return 1
+        } else {
+          return 0
+        }
+      }
+
+      function pizzaVegetable(vegetable) {
+        if (vegetable === 4) {
+          return 2
+        } else if (vegetable === 3) {
+          return 2
+        } else if (vegetable === 2) {
+          return 1
+        } else {
+          return 0
+        }
+      }
+
+      function pizzaSauce(sauce) {
+        if (sauce === 2) {
+          return 2
+        } else {
+          return 1
+        }
+      }
       var checkout = pizzaSize(inputSize) + pizzaMeat(inputMeat) + pizzaVegetable(inputVegetable) + pizzaSauce(inputSauce);
       // return pizzaTotal
     }
