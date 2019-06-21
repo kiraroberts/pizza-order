a536bc404c0e941479ed49d6b7fb247b1e1fbc10
Kira <rosehill1788@protonmail.com>
2019-06-21 10:16:26 -0700

    Add order object and if else logic for pricing

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+// BACKEND LOGIC
+function Pizza() {
+  this.size
+  this.meat
+  this.vegetable
+  this.sauce
+}
+
+function pizzaSize(size) {
+  if {size === 1) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+function pizzaMeat(meat) {
+  if (meat === 3) {
+    return 3
+  } else if (meat === 2) {
+    return 2
+  } else if (meat === 1) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+function pizzaVegetable(vegetable) {
+  if (vegetable === 3) {
+    return 2
+  } else if (vegetable === 2) {
+    return 2
+  } else if (vegetable === 1) {
+    return 1
+  } else {
+    return 0
+  }
+}
