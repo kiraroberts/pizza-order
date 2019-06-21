8ac0cb4c1baed59e3a0b46430a11905a2da5dd2f
Kira <rosehill1788@protonmail.com>
2019-06-21 14:58:31 -0700

    Add math to prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,6 @@
 // BACKEND LOGIC
 
 
-var pizzaOrder = new Pizza(0, 0, 0, 0)
-
-function Pizza(size, meat, vegetable, sauce) {
-  this.size = 0,
-  this.meat = 0,
-  this.vegetable = 0,
-  this.sauce = 0
-
-}
-
-function pizzaSize(size) {
-  if (size === 2) {
-    return 4
-  } else {
-    return 0
-  }
-}
-
-function pizzaMeat(meat) {
-  if (meat === 4) {
-    return 2
-  } else if (meat === 3) {
-    return 2
-  } else if (meat === 2) {
-    return 1
-  } else {
-    return 0
-  }
-}
-
-function pizzaVegetable(vegetable) {
-  if (vegetable === 4) {
-    return 2
-  } else if (vegetable === 3) {
-    return 2
-  } else if (vegetable === 2) {
-    return 1
-  } else {
-    return 0
-  }
-}
-
-function pizzaSauce(sauce) {
-  if (sauce === 2) {
-    return 2
-  } else {
-    return 1
-  }
-}
-
-var pizzaTotal = 0
-var checkout = pizzaTotal;
-
-Pizza.prototype.price = function() {
-  var addSize = pizzaTotal + pizzaSize(inputSize);
-  var addMeat = pizzaTotal + pizzaMeat(inputMeat);
-  var addVegetable = pizzaTotal + pizzaVegetable(inputVegetable);
-  var addSauce = pizzaTotal + pizzaSauce(inputSauce);
-  return pizzaTotal
-}
-
-console.log(pizzaTotal);
 
 // FRONT BACKEND
 
@@ -74,6 +12,67 @@ $(document).ready(function() {
     var inputMeat = parseInt($("#meat").val());
     var inputVegetable = parseInt($("#vegetable").val());
     var inputSauce = parseInt($("#sauce").val());
+
+    var pizzaOrder = new Pizza(0, 0, 0, 0)
+
+    var testPizza = new Pizza(2, 4, 2, 2)
+
+    function Pizza(size, meat, vegetable, sauce) {
+      this.size = size,
+      this.meat = meat,
+      this.vegetable = vegetable,
+      this.sauce = sauce
+
+    }
+
+    function pizzaSize(size) {
+      if (size === 2) {
+        return 4
+      } else {
+        return 0
+      }
+    }
+
+    function pizzaMeat(meat) {
+      if (meat === 4) {
+        return 2
+      } else if (meat === 3) {
+        return 2
+      } else if (meat === 2) {
+        return 1
+      } else {
+        return 0
+      }
+    }
+
+    function pizzaVegetable(vegetable) {
+      if (vegetable === 4) {
+        return 2
+      } else if (vegetable === 3) {
+        return 2
+      } else if (vegetable === 2) {
+        return 1
+      } else {
+        return 0
+      }
+    }
+
+    function pizzaSauce(sauce) {
+      if (sauce === 2) {
+        return 2
+      } else {
+        return 1
+      }
+    }
+
+    var pizzaTotal = 0
+    var checkout = pizzaTotal;
+
+    Pizza.prototype.price = function(pizzaOrder) {
+      var checkout = pizzaSize(inputSize) + pizzaMeat(inputMeat) + pizzaVegetable(inputVegetable) + pizzaSauce(inputSauce);
+      // return pizzaTotal
+    }
+    console.log(checkout)
 
     $("#output").text(checkout);
   });
