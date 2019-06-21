addc6c045748fb92c22cd6bc624b8ccd33ed0cb5
Kira <rosehill1788@protonmail.com>
2019-06-21 11:51:59 -0700

    Add front end logic, html buttons, links to css and javascript folders

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
 // BACKEND LOGIC
 
-
-function Pizza() {
-  this.size
-  this.meat
-  this.vegetable
-  this.sauce
+function Pizza(size, meat, vegetable, sauce) {
+  this.size = 0,
+  this.meat = 0,
+  this.vegetable = 0,
+  this.sauce = 0
 }
 
+var pizzaOrder = new Pizza()
+
 function pizzaSize(size) {
-  if {size === 1) {
-    return 1
+  if (size === 2) {
+    return 4
   } else {
     return 0
   }
 }
 
 function pizzaMeat(meat) {
-  if (meat === 3) {
-    return 3
-  } else if (meat === 2) {
+  if (meat === 4) {
     return 2
-  } else if (meat === 1) {
+  } else if (meat === 3) {
+    return 2
+  } else if (meat === 2) {
     return 1
   } else {
     return 0
@@ -29,11 +30,11 @@ function pizzaMeat(meat) {
 }
 
 function pizzaVegetable(vegetable) {
-  if (vegetable === 3) {
+  if (vegetable === 4) {
+    return 2
+  } else if (vegetable === 3) {
     return 2
   } else if (vegetable === 2) {
-    return 2
-  } else if (vegetable === 1) {
     return 1
   } else {
     return 0
@@ -65,5 +66,12 @@ console.log(pizzaTotal);
 $(document).ready(function() {
   $("#formOrder".submit(function(event) {
     event.preventDefault();
-  });
+
+    var inputSize = parseInt($("#size").val());
+    var inputMeat = parseInt($("#meat").val());
+    var inputVegetable = parseInt($("#vegetable").val());
+    var inputSauce = parseInt($("#sauce").val());
+
+    $("#output").text(checkout);
+  }));
 });
