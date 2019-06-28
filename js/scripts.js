40ca51d1af9b2a0f58d30b0c21099529997f31eb
Kira <rosehill1788@protonmail.com>
2019-06-27 20:39:29 -0700

    Add styling, background image, html inline elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
 // BACKEND LOGIC
 var pizzaOrder = new Pizza();
 
-  function Pizza(size, meat, vegetable, sauce) {
-    this.size = size,
-    this.meat = meat,
-    this.vegetable = vegetable,
-    this.sauce = sauce
-  }
+function Pizza(size, meat, vegetable, sauce) {
+  this.size = size,
+  this.meat = meat,
+  this.vegetable = vegetable,
+  this.sauce = sauce
+}
 
-
-  Pizza.prototype.price = function (size, meat, vegetable, sauce) {
-    var checkout = size + meat + vegetable + sauce;
-    return checkout;
-  };
+Pizza.prototype.price = function (size, meat, vegetable, sauce) {
+  var checkout = size + meat + vegetable + sauce;
+  return checkout;
+};
 
 
 // FRONTEND LOGIC
@@ -21,7 +20,7 @@ $(document).ready(function() {
   $("#formOrder").submit(function(event) {
     event.preventDefault();
 
-    var inputSize = parseInt($("select#size").val());
+    var inputSize = parseInt($("#size").val());
     var inputMeat = parseInt($("#meat").val());
     var inputVegetable = parseInt($("#vegetable").val());
     var inputSauce = parseInt($("#sauce").val());
